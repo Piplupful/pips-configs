088e4e88546769510d0b5b5628e1c28ae2f4ca5b
Piplupful <piplupful@gmail.com>
2026-05-24 10:47:44 -0400

    Move aliases to the bottom of rc.sh

--- rc.sh
+++ rc.sh
@@ -1,8 +1,3 @@
-# Aliases
-alias ll="ls -lha"
-alias df="df -h"
-alias shutdown-now="shutdown -P 0"
-
 # Export ENVs
 #  Golang
 export PATH=$PATH:/usr/local/go/bin
@@ -11,3 +6,8 @@ export PATH=$PATH:$HOME/.cargo/bin
 
 # Prints
 pokeget random --hide-name | fastfetch --file-raw -
+
+# Aliases
+alias ll='ls -lh'
+alias df='df -h'
+alias shutdownNow='shutdown -P 0'
